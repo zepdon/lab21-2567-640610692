30ad89a8db9014a11f5e83f4ae947f90fbc398ba
Poon <zeepengineer@gmail.com>
2024-10-05 18:33:51 +0700

    database URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,8 +80,8 @@ export default function RootLayout({
               </Title>
               {children}
               <Footer
-                studentId="660610999"
-                fullName="Dome Potikanond"
+                studentId="640610692"
+                fullName="Poonnapat Panumonwatee"
                 year="2024"
               />
             </Container>
